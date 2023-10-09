d3a2c94c75ba2d8765c67f24ddd041398e59266a
Alexander Yalcin <46671361+AlexanderYalcin@users.noreply.github.com>
2023-10-09 22:18:05 +0200

    Added color variables

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -2,25 +2,19 @@ const API_KEY = process.env.TMDB_API_KEY;
 const BASE_URL = process.env.TMDB_BASE_URL;
 
 export const getTrendingMovies = async () => {
-  const res = await fetch(
-    `${BASE_URL}/trending/movie/day?language=en-US&api_key=${API_KEY}`
-  );
+  const res = await fetch(`${BASE_URL}/trending/movie/day?api_key=${API_KEY}`);
   const data = await res.json();
   return data.results;
 };
 
 export const getNowPlayingMovies = async () => {
-  const res = await fetch(
-    `${BASE_URL}/movie/now_playing?language=en-US&page=1&api_key=${API_KEY}`
-  );
+  const res = await fetch(`${BASE_URL}/movie/now_playing?api_key=${API_KEY}`);
   const data = await res.json();
   return data.results;
 };
 
 export const getTopRatedMovies = async () => {
-  const res = await fetch(
-    `${BASE_URL}/movie/top_rated?language=en-US&page=1&api_key=${API_KEY}`
-  );
+  const res = await fetch(`${BASE_URL}/movie/top_rated?api_key=${API_KEY}`);
   const data = await res.json();
   return data.results;
 };
